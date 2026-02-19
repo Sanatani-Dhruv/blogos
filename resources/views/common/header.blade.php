6184ff5470e67b69b8fadb1f5ca53daec2ad49a0
Sanatani-Dhruv <dhruvdds125@gmail.com>
2026-02-19 22:40:30 +0530

    Created Migration file for blogs not completed the file, some style changes also

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -9,16 +9,16 @@
         </div>
         <nav class="navbar">
             <ul class="flex flex-1 gap-4 text-lg flex-col font-medium md:flex-row max-w-screen-lg" >
-                <a class="navlink-li hover:bg-red-300 transition-all pt-1 pb-1 pl-2 pr-2 rounded-2xl text-center" href="/contact">
+                <a class="navlink-li hover:bg-red-300 transition-all pt-1 pb-1 pl-3 pr-3 rounded-md text-center" href="/contact">
                     <li class="navlink">Contact</li>
                 </a>
-                <a class="navlink-li hover:bg-red-300 transition-all pt-1 pb-1 pl-2 pr-2 rounded-2xl text-center" href="/about">
+                <a class="navlink-li hover:bg-red-300 transition-all pt-1 pb-1 pl-3 pr-3 rounded-md text-center" href="/about">
                     <li class="navlink">About</li>
                 </a>
-                <a class="navlink-li hover:bg-red-300 transition-all pt-1 pb-1 pl-2 pr-2 rounded-2xl text-center" href="/signup">
+                <a class="navlink-li hover:bg-red-300 transition-all pt-1 pb-1 pl-3 pr-3 rounded-md text-center" href="/signup">
                     <li class="navlink">Sign Up</li>
                 </a>
-                <a class="navlink-li bg-red-300 hover:rounded-none transition-all pt-1 pb-1 pl-2 pr-2 rounded-2xl text-center shadow hover:shadow-blue-900/100 shadow-md shadow" href="/signin">
+                <a class="navlink-li bg-red-300 hover:rounded-none transition-all pt-1 pb-1 pl-3 pr-3 rounded-md text-center shadow hover:shadow-blue-900/100 shadow-md shadow" href="/signin">
                     <li class="navlink">Sign In</li>
                 </a>
             </ul>
