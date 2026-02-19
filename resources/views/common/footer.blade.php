b82d3c68aab3e58d0bcbcd5a4d6128bba0c9c0ca
Sanatani-Dhruv <dhruvdds125@gmail.com>
2026-02-19 14:44:28 +0530

    Now a Simple Home Page Setup in which header and footer view setup which shows navbars

---
+++ resources/views/common/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-blue-400 footer">
+    <div class="text-center ml-8 mr-8 pt-4 pb-4 box-border">
+        <div class="h-full text-lg font-semibold">
+            <h1>
+                © 2026 Blogos - Built with Laravel and ❤️
+            </h1>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,25 @@
+<header class="fixed top-0 left-0 right-0 bg-blue-300 header">
+    <div class="ml-8 mr-8 pt-4 pb-4 flex md:items-center justify-between box-border ">
+        <div class="h-full">
+            <h1 class="text-2xl font-semibold">
+                Blogos
+            </h1>
+        </div>
+        <nav class="navbar">
+            <ul class="flex flex-1 gap-4 text-lg flex-col font-medium md:flex-row max-w-screen-lg" >
+                <li class="navlink-li hover:bg-red-300 transition-all pt-1 pb-1 pl-2 pr-2 rounded-2xl text-center">
+                    <a class="navlink" href="/">Home</a>
+                </li>
+                <li class="navlink-li hover:bg-red-300 transition-all pt-1 pb-1 pl-2 pr-2 rounded-2xl text-center">
+                    <a class="navlink" href="/contact">Contact</a>
+                </li>
+                <li class="navlink-li hover:bg-red-300 transition-all pt-1 pb-1 pl-2 pr-2 rounded-2xl text-center">
+                    <a class="navlink" href="/about">About</a>
+                </li>
+                <li class="navlink-li hover:bg-red-300 transition-all pt-1 pb-1 pl-2 pr-2 rounded-2xl text-center">
+                    <a class="navlink" href="/register">Register</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
