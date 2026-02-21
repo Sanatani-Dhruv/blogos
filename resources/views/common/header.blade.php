439873a8e2bba1c508b78737e1d68224cf680147
Dhruv <24amtics067@gmail.com>
2026-02-21 11:07:06 +0530

    Removed Hell Alpine Code which was messing, trying to do what other than that

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,13 +1,4 @@
 <header class="fixed top-0 left-0 right-0 bg-blue-300/100 header z-10 shadow shadow-lg">
-    <style>
-.show {
-    display: block;
-}
-
-.noshow {
-    display: none;
-}
-    </style>
     <div class="ml-8 mr-8 pt-4 pb-4 flex md:items-center justify-between box-border">
         <div class="h-full">
             <a href="/">
@@ -16,9 +7,9 @@
                 </h1>
             </a>
         </div>
-        <div x-data="{ open:true }">
-            <img width="18" @click="open = !open;" :class="open ? 'show' : 'noshow';" class="cursor-pointer " src="/home-icon.svg" alt="">
-            <nav :class="open ? 'noshow' : 'show';" @click.outside="open = false" class="navbar flex">
+        <div>
+            <img width="18" class="cursor-pointer block md:hidden" src="/home-icon.svg" alt="">
+            <nav class="navbar hidden md:block">
                 <ul class="flex flex-1 gap-4 text-lg flex-col font-medium md:flex-row max-w-screen-lg" >
                     @if ( request()->path() != '/')
                         <a class="navlink-li hover:bg-red-300 align-middle transition-all pt-1 pb-1 pl-3 pr-3 rounded-md text-center" href="/">
